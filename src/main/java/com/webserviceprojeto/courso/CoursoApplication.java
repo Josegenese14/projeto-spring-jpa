781e1dd6ba6851339994f728778ee98d9699d7dd
Josegenese14 <jgenese6@gmail.com>
2023-04-24 20:08:17 -0300

    Inicio do Projeto

---
+++ src/main/java/com/webserviceprojeto/courso/CoursoApplication.java
@@ -0,0 +1,13 @@
+package com.webserviceprojeto.courso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webserviceprojeto/courso/CoursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webserviceprojeto.courso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
